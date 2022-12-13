0076c396756da18c24d166e2333a098c738d8075
Anthony Siletti <asiletti@icloud.com>
2022-12-13 16:53:45 -0500

    disable mongostore

--- server.js
+++ server.js
@@ -38,16 +38,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // mount the session middleware
-app.use(
-  session({
-    store: MongoStore.create({
-      mongoUrl: process.env.DATABASE_URL,
-    }),
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+// app.use(
+//   session({
+//     store: MongoStore.create({
+//       mongoUrl: process.env.DATABASE_URL,
+//     }),
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
 
 app.use(passport.initialize());
 app.use(passport.session());
