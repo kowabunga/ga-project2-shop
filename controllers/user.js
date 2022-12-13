0f0bc4eea8d4de359f9ca22274a8b1fb8fd47868
Anthony Siletti <asiletti@icloud.com>
2022-12-13 10:09:44 -0500

    fix user pages

--- controllers/user.js
+++ controllers/user.js
@@ -7,8 +7,6 @@ async function index(req, res) {
     const orders = await Order.find({ user: user._id }).populate(
       'products.product'
     );
-    console.log(user);
-    console.log(orders);
     res.render('user/index', { user, orders });
   } catch (error) {
     console.log(error);
@@ -19,8 +17,16 @@ async function index(req, res) {
 // This goes here beacuse it's technically under /user/orders/:id route but we'll just render the order/show page since we're going to show the same thing
 async function show(req, res) {
   try {
-    const order = await Order.findById(req.params.id);
-    res.render('order/show', { order, isUserRoute: true });
+    const order = await Order.findById(req.params.id).populate(
+      'products.product'
+    );
+
+    let total = 0;
+    order.products.forEach(
+      product => (total += product.product.price * product.count)
+    );
+
+    res.render('orders/show', { order, isUserRoute: true, total });
   } catch (error) {
     console.log(error);
     res.status(500).send('Something went wrong, check the terminal...');
